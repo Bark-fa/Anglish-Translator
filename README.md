414351337329d6004b5c1d36733029299db7a564
Fady Bark <fadibark@outlook.com>
2020-10-11 14:07:29 +0300

    added support for special characters

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 An English to Anglish (fully Germanic English) translator
 
 You can help by adding words to the wordbook.js file; all words are taken from The Anglish Moot: http://anglish.fandom.com/wiki/English_Wordbook
-The words must be added in alphabetical order for easier maintenance
+The words must be added in alphabetical order for easier maintenance, some words and definitions are altered for clarity and technical purposes.
 
 Community content from fandom is available under CC-BY-SA unless otherwise noted.
 CC-BY-SA summary: https://creativecommons.org/licenses/by-nc/3.0/
--- script.js
+++ script.js
@@ -6,30 +6,64 @@ function capitaliseFirstLetter(string) {
 }
 
 
-const vowels = ['a', 'e', 'i', 'o', 'u']
-
-
 window.onload = function () {
 
     // set up the constants.
     const translateBtn = document.getElementById("translate")
     const english = document.getElementById("english")
     const anglish = document.getElementById("anglish")
+    const vowels = ['a', 'e', 'i', 'o', 'u'];
+    const specialCharacters = /[ `!@#$%^&*,.;?~]/;
 
-    
     translateBtn.onclick = function () {
-        
+
         // Set up the variables.
         let englishText = english.value;
         let anglishText = ""
+        let keys = [];
+        let specialCharactersIndex = {};
 
-        
+
         // Split the English text into seperate words based on the space character, and make the text all lowercase.
         let englishWords = englishText.split(" ").map(function (f) {
             return f.toLowerCase();
         });
 
+        // Check if word contains special characters, if so handle them.
+
+        englishWords.forEach((word, index) => {
+
+            if (specialCharacters.test(word)) {
+                let character = word.match(specialCharacters);
+                Object.assign(specialCharactersIndex, {
+                    [index]: [character]
+                });
+                englishWords[index] = word.replace(/[ `!@#$%^&*,.;?~]/g, '');
+
+            }
+
+        });
+        for (var k in specialCharactersIndex) keys.push(k);
         
+        
+
+        // Some words may require exceptions, capital city is one of them; i'll figure out a better way once we have more words that need exceptions, this is temporary.
+        englishWords.forEach((word, index) => {
+
+            if (word.toLowerCase() == "capital") {
+
+                if (englishWords[index + 1] != undefined && englishWords[index + 1] == "city") {
+
+                    englishWords[index] = "headburg"
+                    englishWords[index + 1] = ""
+
+                }
+
+            }
+        });
+
+        
+
         // Check if an English word has an Anglish translation, if so swap the English word with its Anglish match.
         englishWords.forEach((word, index) => {
 
@@ -39,16 +73,16 @@ window.onload = function () {
 
         });
 
-        
+
         // Decide if "a" or "an" should be used.
         englishWords.forEach((word, index) => {
 
             if (word.toLowerCase() == "an" || word.toLowerCase() == "a") {
 
-                if (englishWords[index + 1][0] in vowels) {
-                    
+                if (englishWords[index + 1] != undefined && englishWords[index + 1][0] in vowels) {
+
                     englishWords[index] = "an"
-                    
+
                 } else {
 
                     englishWords[index] = "a"
@@ -57,14 +91,25 @@ window.onload = function () {
 
         });
 
-        
+
+        // Re add special characters
+        englishWords.forEach((word, index) => {
+
+            if (index in keys) {
+                console.log(index);
+                console.log(keys);
+                console.log(specialCharactersIndex)
+                englishWords[keys[index]] += specialCharactersIndex[keys[index]][0][0];
+            }
+        });
+
         // Parse the translated words into a string, and add spacing between words.
         englishWords.forEach((word, index) => {
 
             if (index == 0) {
-                
+
                 anglishText += word;
-                
+
             } else {
 
                 anglishText += " " + word;
@@ -73,8 +118,9 @@ window.onload = function () {
         });
 
 
+
         // Display the translated text.
         anglish.textContent = capitaliseFirstLetter(anglishText);
 
-    }    
+    }
 }
